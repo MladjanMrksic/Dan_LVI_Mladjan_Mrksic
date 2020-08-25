b83ca5f2d78649b391c24b79c5e60e9503431e26
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-25 12:59:46 +0200

    Added method to compress files

---
+++ HTML-ZIPApp/Validation/URLValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTML_ZIPApp.Validation
+{
+    class URLValidation
+    {
+        public static bool ValidateURl(string address)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(address, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
--- HTML-ZIPApp/ViewModel/MainWindowViewModel.cs
+++ HTML-ZIPApp/ViewModel/MainWindowViewModel.cs
@@ -1,18 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HTML_ZIPApp.Command;
+using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Input;
+using HTML_ZIPApp.Validation;
+using System.IO.Compression;
 
 namespace HTML_ZIPApp.ViewModel
 {
     class MainWindowViewModel
     {
         MainWindowView view;
-
+        StreamWriter sw;
         public MainWindowViewModel(MainWindowView mwv)
         {
             view = mwv;
+        }
+
+        private ICommand close;
+        public ICommand Close
+        {
+            get
+            {
+                if (close == null)
+                {
+                    close = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return close;
+            }
+        }
+        private void CloseExecute()
+        {
+            try
+            {
+                view.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception" + ex.Message.ToString());
+            }
+        }
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+
+        private ICommand getHTML;
+        public ICommand GetHTML
+        {
+            get
+            {
+                if (getHTML == null)
+                {
+                    getHTML = new RelayCommand(param => GetHTMLExecute(), param => CanGetHTMLExecute());
+                }
+                return getHTML;
+            }
+        }
+        private void GetHTMLExecute()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string site = view.txtSite.Text;
+                    string fileName = string.Format(DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_" + DateTime.Now.Hour + "_" + DateTime.Now.Minute);
+                    string filePath = string.Format(".../.../HTMLAddresses/" + fileName + ".html");
+                    File.Create(filePath).Close();
+                    client.DownloadFile(site, filePath);
+                    MessageBox.Show("URL successfully downloaded.\nCheck HTMLAddresses folder","Success!",MessageBoxButton.OK,MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception" + ex.Message.ToString());
+            }
+        }
+        private bool CanGetHTMLExecute()
+        {
+           return URLValidation.ValidateURl(view.txtSite.Text);
+        }
+
+        private ICommand zipHTML;
+        public ICommand ZipHTML
+        {
+            get
+            {
+                if (zipHTML == null)
+                {
+                    zipHTML = new RelayCommand(param => ZipHTMLExecute(), param => CanZipHTMLExecute());
+                }
+                return zipHTML;
+            }
+        }
+        private void ZipHTMLExecute()
+        {
+            try
+            {
+                DirectoryInfo directorySelected = new DirectoryInfo(".../.../HTMLAddresses/");
+                Compress(directorySelected);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception" + ex.Message.ToString());
+            }
+        }
+        private bool CanZipHTMLExecute()
+        {
+            return true;
+        }
+
+        public static void Compress(DirectoryInfo directorySelected)
+        {
+            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+            {
+                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                {
+                    if ((File.GetAttributes(fileToCompress.FullName) &
+                       FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                    {
+                        using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                        {
+                            using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                               CompressionMode.Compress))
+                            {
+                                originalFileStream.CopyTo(compressionStream);
+                            }
+                        }
+                        FileInfo info = new FileInfo(".../.../HTMLAddresses/" + Path.DirectorySeparatorChar + fileToCompress.Name + ".gz");
+                        MessageBox.Show($"Compressed {fileToCompress.Name} from {fileToCompress.Length.ToString()} to {info.Length.ToString()} bytes.");
+                    }
+                }
+            }
         }
     }
 }
